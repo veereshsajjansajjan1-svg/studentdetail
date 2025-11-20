0ed49ca3393101c553ad7217227be68319a8315b
veereshsajjansajjan1-svg <veereshsajjansajjan1@gmail.com>
2025-11-20 14:43:37 +0530

    Fix indentation and improve print statements

--- studentdetails.py
+++ studentdetails.py
@@ -1,12 +1,13 @@
 import sys
+
 if len(sys.argv) != 3:
-   print("Usage: python student.py <name> <roll>")
-   sys.exit(1)
+    print("Usage: python student.py <name> <roll>")
+    sys.exit(1)
 
-        script_name = sys.argv[0]
-  name = sys.argv[1]
-  rollno = sys.argv[2]
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
 
-print("script Name:", script_name)
-print("student Name:", name)
+print("Script Name:", script_name)
+print("Student Name:", name)
 print("Roll Number:", rollno)
