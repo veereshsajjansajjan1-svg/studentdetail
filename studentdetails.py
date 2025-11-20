45e38229a8769b8887de9d1fd4f8bb4a62facfa0
veereshsajjansajjan1-svg <veereshsajjansajjan1@gmail.com>
2025-11-20 14:10:07 +0530

    Create studentdetails.py

---
+++ studentdetails.py
@@ -0,0 +1,12 @@
+import sys
+if len(sys.argv) != 3:
+   print("Usage: python student.py <name> <roll>",
+   sys.exit(1)
+
+        script_name = sys.argv[0]
+  name = sys.argv[1]
+  rollno = sys.argv[2]
+
+print("script Name:", script_name)
+print("student Name:", name)
+print("Roll Number:", rollno)
