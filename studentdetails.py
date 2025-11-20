e48d60cb5569c1aa7e2ae12bbdbaefd19c20eb81
veereshsajjansajjan1-svg <veereshsajjansajjan1@gmail.com>
2025-11-20 14:39:59 +0530

    Update studentdetails.py

--- studentdetails.py
+++ studentdetails.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) != 3:
-   print("Usage: python student.py <name> <roll>",
+   print("Usage: python student.py <name> <roll>")
    sys.exit(1)
 
         script_name = sys.argv[0]
